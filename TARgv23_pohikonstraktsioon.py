f4ef54e9d5ee8789eb4ab4a53deb2ec45ec5e9fb
opilane <opilane@HP-CZC2349HTC>
2023-11-16 12:35:43 +0200

    tehed

--- TARgv23_pohikonstraktsioon.py
+++ TARgv23_pohikonstraktsioon.py
@@ -1,6 +1,11 @@
-print("Tere tulemast")
-kool=input("Mis koolis sa opid?: ") #str kool
-kursus=int(input("Mis kuursusel?: ")) #int kursus
-print("Tere tulemast kooli "+kool+"\nOle hea "+str(kursus)+".kursuse opilaseks!"
-print("Tere tulemast koole", kool, "!\nOle hea", kursus,".kursuse opilaseks!")
-print("Tere tulemast kooli {0}!\nOle hea {1}. kuursuse opetajaks!",.format(kool,kursus))
+print("Tere tulemast".center(50))
+kool = input("\tMis koolis sa opid?: ").capitalize()  # str Kool
+kursus = int(input("\tMis kuursusel?: "))  # int kursus
+print("Tere tulemast kooli " + kool.upper() + "\nOle hea " + str(kursus) + ".kursuse opilaseks!")  # kool on KOOL
+print("Tere tulemast kooli", kool.lower(), "!\nOle hea", kursus, ".kursuse opilaseks!")  # kool="tthk"
+print("Tere tulemast kooli {0}!\nOle hea {1}. kuursuse opetajaks!".format(kool, kursus))  # kool="tthk"
+print(type(kool))
+print(type(kursus))
+
+arv1=float(input("Arv 1: "))
+arv2=float(input("Arv 2: "))
