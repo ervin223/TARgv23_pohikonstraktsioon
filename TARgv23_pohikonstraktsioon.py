e108bde33fda3cb0dd8041a33f92b6456fa94ba7
opilane <opilane@HP-CZC2349HTC>
2023-11-16 15:45:12 +0200

    Iseseisevtoo

--- TARgv23_pohikonstraktsioon.py
+++ TARgv23_pohikonstraktsioon.py
@@ -1,81 +1,114 @@
-﻿from math import *
-from xml.etree.ElementTree import PI
+﻿#1 ülesanne
+print("Tere, maailm!")
+name = input("Kirjuta oma nimi: ").capitalize()
+print(f'Tere, maailm! Tervitan sind {name}')
 
-print("Tere tulemast".center(50))
-kool = input("\tMis koolis sa opid?: ").capitalize()  # str Kool
-kursus = int(input("\tMis kuursusel?: "))  # int kursus
-print("Tere tulemast kooli " + kool.upper() + "\nOle hea " + str(kursus) + ".kursuse opilaseks!")  # kool on KOOL
-print("Tere tulemast kooli", kool.lower(), "!\nOle hea", kursus, ".kursuse opilaseks!")  # kool="tthk"
-print("Tere tulemast kooli {0}!\nOle hea {1}. kuursuse opetajaks!".format(kool, kursus))  # kool="tthk"
-print(type(kool))
-print(type(kursus))
+age = input("Kui vana sa oled? ").strip()
+print(f'Tere, maailm! Tervitan sind {name}! Sa oled {age} aastat vana.')
 
-arv1=float(input("Arv 1: "))
-arv2=float(input("Arv 2: ")) 
-print("Summa {0} + {1}={2}".format(arv1,arv2,arv1+arv2))
-print("Summa {0} - {1}={2}".format(arv1,arv2,arv1+arv2))
-print("Summa {0} * {1}={2}".format(arv1,arv2,arv1+arv2))
-print("Summa {0} / {1}={2}".format(arv1,arv2,arv1+arv2))
-print("Summa {0} astmes {1} = {2}".format(arv1,arv2,arv1+arv2))
-print("Summa {0} ja {1} jääk = {2}".format(arv1,arv2,arv1+arv2))
-print("Summa {0} ja {1} jagamise täis osa {2}".format(arv1,arv2,arv1+arv2))
-
-tehe=input("Mida teha: ")
-v=eval(str(arv1)+tehe+str(arv2))
-print(v)
-
-
-print("Tere maailm")
-nimi = input("Mis on sinu nimi")
-print("Tere maailm! Tervitan sind {nimi}!".format(nimi)) #{переменная}
-
-vanus = input("Kirjuta vanus")
-print("Tere, maailm! Tervitan sind {nimi}! Sa oled {vanus} aastat vana".format(nimi, vanus))
-
-#2
+#2 ülesanne
 vanus = 18 #int
 eesnimi = "Jaak" #str
 pikkus = 16.5 #float
-kes_kaib_koolis = True #bool 
+kas_käib_koolis = True #bool
 
-#3
-komm = 3
-qua = input("how many komm?")
-print("laua peal on {komm}".format(komm))
-komm = qua
-print("nüüd laua peal on {komm}".format(qua))
 
-#4
-c=float(input("ümbermõtt"))
-d=c/pi 
-print("Läbimõtt",d)
+#3 ülesanne
 
-#5
-a=float(input("Esimene kateet"))
-b=float(input("Teine kateet"))
-d1=hypot(a,b)
-print("d=",d)
-print("d1=",d1)
+count = int(input("Kui palju kommi on laua peal?"))
+ate = int(input("Kui palju kommi sa sõid?"))
+new = count - ate
+print(f'Laua peal on veel {new} kommi.')
 
-try:
-    a=int(input("a"))
-except:
-    print("Andmetüübi viga!") #если неверно, то...
 
-#7
-a1=randint(1,10)
-a2=randint(1,10)
-a3=randint(1,10)
-a4=randint(1,10)
-a5=randint(1,10)
-print("arvude {a1},{a2},{a3},{a4} ja aretmeetiline keskmine {a5}".format({a1},{a2},{a3},{a4},{a5}))
+#4 ülesanne
 
-#8
+
+c = int(input("Mis on puu läbimõõdu? "))
+d = (c/(math.pi))
+print(d)
+
+
+#5 ülesanne
+
+n = int(input())
+m = int(input())
+d = ((n**2)+(m**2))**0.5
+print(d)
+
+
+#6 ülesanne
+
+
+aeg = int(input("Mitu tundi kulus sõiduks? "))
+teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
+kiirus = teepikkus / aeg
+
+print("Sinu kiirus oli", int(kiirus), "km/h")
+
+
+#7 ülesanne
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+avg = (a+b+c+d+e)/5
+print(avg)
+
+
+#8 ülesanne
+
+
 print("    @..@")
 print("   (----)")
 print("  ( \__/ )")
-print(" ^^ "" ^^ ")
+print('  ^^ "" ^^' )
 
-#9
 
-#10
+#9 ülesanne
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+p = a + b +c 
+print(p)
+
+
+#10 ülesanne
+cost = 12.9
+print((cost*1.1)/2)
+
+
+from math import *
+print("Ruudu karakteristikud")
+a=int(input('Sisesta ruudu kulje pikkus => '))
+S=float(a**2)
+print("Ruudu pindala", S)
+P=float(4*a)
+print("Ruudu umbermoot", P)
+di=(2*(a**2))**0.5
+print("Ruudu diagonaal", round(di,2))
+print()
+print("Ristkuliku karakteristikud")
+b=int(input("Sisesta ristkuliku 1. kulje pikkus => "))
+c=int(input("Sisesta ristkuliku 2. kulje pikkus => "))
+S=b*c
+print('Ristkuliku pindala', S)
+P=2*(b+c)
+print("Ristkuliku umbermoot", P)
+di=(b**2+c**2)**0.5
+print("Ristkuliku diagonaal", round(di, 1))
+print()
+print("Ringi karakteristikud")
+r=float(input('Sisesta ringi raadiusi pikkus => '))
+d=2*r
+print("Ringi labimoot", d)
+S = math.pi*(r**2)
+print("Ringi pindala", round(S,2))
+C= math.pi * r * 2
+print("Ringjoone pikkus", round(C, 2))
