8e108eb17954da257491ec5ca726c7aa79f78800
opilane <opilane@HP-CZC2349HTC>
2023-11-16 11:46:46 +0200

    Add project files.

---
+++ TARgv23_pohikonstraktsioon.py
@@ -0,0 +1,6 @@
+print("Tere tulemast")
+kool=input("Mis koolis sa opid?: ") #str kool
+kursus=int(input("Mis kuursusel?: ")) #int kursus
+print("Tere tulemast kooli "+kool+"\nOle hea "+str(kursus)+".kursuse opilaseks!"
+print("Tere tulemast koole", kool, "!\nOle hea", kursus,".kursuse opilaseks!")
+print("Tere tulemast kooli {0}!\nOle hea {1}. kuursuse opetajaks!",.format(kool,kursus))
